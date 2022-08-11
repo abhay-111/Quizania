49f8ead4624a4c134356a19c80d4773fd90770ed
abhay-111 <abhay11211@gmail.com>
2022-08-11 09:15:53 +0530

    feat:Login and signup conditionally rendered completed

--- src/components/forms/Login.jsx
+++ src/components/forms/Login.jsx
@@ -1,5 +1,58 @@
+import { Flex, Input } from "@chakra-ui/react";
 import React from "react";
-
+import { useState } from "react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Divider,
+  Text,
+} from "@chakra-ui/react";
 export default function Login() {
-  return <div>Login</div>;
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  const [formData, setFomData] = useState({
+    email: "",
+    password: "",
+  });
+  //function to set the data entered by the user
+  const handleFormData = (event) => {
+    setFomData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <Flex>
+      <FormControl>
+        <Divider mb={4}></Divider>
+        <FormLabel mt={3}>Email address</FormLabel>
+        <Input type="email" />
+        <FormHelperText>We'll never share your email.</FormHelperText>
+        {/* <Divider mt={3}></Divider> */}
+        <InputGroup mt={4} size="md">
+          <Input
+            pr="4.5rem"
+            type={show ? "text" : "password"}
+            placeholder="Enter password"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+
+        <Button colorScheme="purple" w="100%" mt={5}>
+          Submit
+        </Button>
+      </FormControl>
+    </Flex>
+  );
 }
--- src/components/forms/SignUp.jsx
+++ src/components/forms/SignUp.jsx
@@ -1,5 +1,6 @@
 import { Flex, Input } from "@chakra-ui/react";
 import React from "react";
+import { useState } from "react";
 import {
   FormControl,
   FormLabel,
@@ -9,26 +10,41 @@ import {
   InputRightElement,
   Button,
   Divider,
+  Text,
 } from "@chakra-ui/react";
 export default function SignUp() {
-  const [show, setShow] = React.useState(false);
+  const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
 
+  const [formData, setFomData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+  //function to set the data entered by the user
+  const handleFormData = (event) => {
+    setFomData({
+      ...formData,
+
+      [event.target.name]: event.target.value,
+    });
+  };
   return (
     <Flex>
       <FormControl>
         <Divider mb={4}></Divider>
         <FormLabel>Full Name</FormLabel>
-        <Input type="text" />
+        <Input name="fullName" onChange={handleFormData} type="text" />
         <FormHelperText>Eg : Jane Doe</FormHelperText>
         <FormLabel mt={3}>Email address</FormLabel>
-        <Input type="email" />
+        <Input name="email" onChange={handleFormData} type="email" />
         <FormHelperText>We'll never share your email.</FormHelperText>
-        {/* <Divider mt={3}></Divider> */}
         <InputGroup mt={4} size="md">
           <Input
             pr="4.5rem"
             type={show ? "text" : "password"}
+            name="password"
+            onChange={handleFormData}
             placeholder="Enter password"
           />
           <InputRightElement width="4.5rem">
